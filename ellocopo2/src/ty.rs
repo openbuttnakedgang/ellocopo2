fb5b18a4f8f3383741544a6c4b25c01536c0f0cb
fmv <mishka798@mail.ru>
2020-03-13 14:27:43 +0300

    pub functions

--- ellocopo2/src/ty.rs
+++ ellocopo2/src/ty.rs
@@ -3,11 +3,13 @@ use core::slice::from_raw_parts;
 
 use num_enum::TryFromPrimitive;
 use num_enum::IntoPrimitive;
+use serde::{Deserialize, Serialize};
 
 /// Available types
 ///
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize)]
 pub enum Value<'a> {
     UNIT(()),
     BOOL(bool),
