0ef50ab2aea2b280866b2b489f20a4097ded7b70
fmv <mishka798@mail.ru>
2020-03-16 19:36:55 +0300

    added new types

--- ellocopo2/src/parser.rs
+++ ellocopo2/src/parser.rs
@@ -1,10 +1,8 @@
-
 use core::convert::TryFrom;
 use core::ops::Range;
 
-use crate::ty::*;
 use crate::protocol::*;
-
+use crate::ty::*;
 
 #[derive(Debug)]
 pub enum ParserError {
@@ -18,13 +16,9 @@ pub enum ParserError {
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
-pub struct Msg<'a> (
-    pub AnswerCode,
-    pub &'a str,
-    pub Value<'a>
-);
+pub struct Msg<'a>(pub AnswerCode, pub &'a str, pub Value<'a>);
 
-pub type ParseResult<'a> = Result<Msg<'a>,ParserError>;
+pub type ParseResult<'a> = Result<Msg<'a>, ParserError>;
 
 pub enum ParseState {
     ParsingHeader,
@@ -42,14 +36,14 @@ pub struct ParseMsg {
 }
 
 impl ParseMsg {
-    pub fn new() -> Self { 
-        ParseMsg { 
-            path: 0 .. 0,
-            payload: 0 .. 0, 
+    pub fn new() -> Self {
+        ParseMsg {
+            path: 0..0,
+            payload: 0..0,
             pos: 0,
             state: ParseState::ParsingHeader,
             header: Default::default(),
-        } 
+        }
     }
 
     pub fn reset(&mut self) {
@@ -57,18 +51,18 @@ impl ParseMsg {
         self.state = ParseState::ParsingHeader;
     }
 
-    pub fn try_parse<'a>(&mut self, i: &'a[u8]) -> ParseResult<'a> {
+    pub fn try_parse<'a>(&mut self, i: &'a [u8]) -> ParseResult<'a> {
         use ParseState::*;
         loop {
             match &self.state {
                 ParsingHeader => {
                     let header = header_parser(i)?;
                     self.header = *header;
-                    if self.header.path_sz as usize > MAX_PATH_SZ { 
-                        return ParseResult::Err(ParserError::BadPathSz); 
+                    if self.header.path_sz as usize > MAX_PATH_SZ {
+                        return ParseResult::Err(ParserError::BadPathSz);
                     }
-                    if self.header.payload_sz as usize > MAX_PAYLOAD_SZ { 
-                        return ParseResult::Err(ParserError::BadPayloadSz); 
+                    if self.header.payload_sz as usize > MAX_PAYLOAD_SZ {
+                        return ParseResult::Err(ParserError::BadPayloadSz);
                     }
 
                     self.state = ParsingPath;
@@ -78,7 +72,7 @@ impl ParseMsg {
                     if i.len() - self.pos < self.header.path_sz as usize {
                         return Err(ParserError::NeedMoreData);
                     }
-                    self.path = self.pos .. self.pos + self.header.path_sz as usize;
+                    self.path = self.pos..self.pos + self.header.path_sz as usize;
 
                     self.state = ParsingValue;
                     self.pos += self.header.path_sz as usize;
@@ -87,15 +81,18 @@ impl ParseMsg {
                     if i.len() - self.pos < self.header.payload_sz as usize {
                         return Err(ParserError::NeedMoreData);
                     }
-                    self.payload = self.pos .. self.pos + self.header.payload_sz as usize;
+                    self.payload = self.pos..self.pos + self.header.payload_sz as usize;
 
                     self.state = ParsingDone;
-                    
-                    let code = AnswerCode::try_from(self.header.code).map_err(|_| ParserError::BadCode)?;
-                    let path_str: & str = unsafe { core::str::from_utf8_unchecked(&i[self.path.clone()]) };
-                    let value: Value = value_parser(& i[self.pos .. self.pos + self.header.payload_sz as usize],
-                        self.header.payload_ty)?;
-                    
+
+                    let code =
+                        AnswerCode::try_from(self.header.code).map_err(|_| ParserError::BadCode)?;
+                    let path_str: &str =
+                        unsafe { core::str::from_utf8_unchecked(&i[self.path.clone()]) };
+                    let value: Value = value_parser(
+                        &i[self.pos..self.pos + self.header.payload_sz as usize],
+                        self.header.payload_ty,
+                    )?;
                     self.reset();
                     return Ok(Msg(code, path_str, value));
                 }
@@ -105,11 +102,10 @@ impl ParseMsg {
     }
 }
 
-
 #[inline(always)]
 fn header_parser(i: &[u8]) -> Result<&Header, ParserError> {
     if i.len() >= HEADER_SZ {
-        let header : &Header = unsafe { &*(i.as_ptr() as *const _)};
+        let header: &Header = unsafe { &*(i.as_ptr() as *const _) };
         Ok(header)
     } else {
         Err(ParserError::BadHeader)
@@ -117,59 +113,108 @@ fn header_parser(i: &[u8]) -> Result<&Header, ParserError> {
 }
 
 #[inline(always)]
-fn value_parser<'a>(payload: &'a[u8], ty_id: u8) -> Result<Value<'a>, ParserError> {
+fn value_parser<'a>(payload: &'a [u8], ty_id: u8) -> Result<Value<'a>, ParserError> {
     use crate::ty::TypeTag::*;
 
     let ty_id = TypeTag::try_from(ty_id).map_err(|_| ParserError::BadTypeID)?;
 
     match ty_id {
-        UNIT => {
-            Ok(Value::UNIT(()))
+        UNIT => Ok(Value::UNIT(())),
+        BOOL => Ok(Value::BOOL({
+            if payload[0] == 0 {
+                false
+            } else {
+                true
+            }
+        })),
+        I32 => Ok(Value::I32({ unsafe { *(payload.as_ptr() as *const _) } })),
+        I16 => Ok(Value::I16({ unsafe { *(payload.as_ptr() as *const _) } })),
+        I8 => Ok(Value::I8({ unsafe { *(payload.as_ptr() as *const _) } })),
+        U32 => Ok(Value::U32({ unsafe { *(payload.as_ptr() as *const _) } })),
+        U16 => Ok(Value::U16({ unsafe { *(payload.as_ptr() as *const _) } })),
+        U8 => Ok(Value::U8({ unsafe { *(payload.as_ptr() as *const _) } })),
+        STR => Ok(Value::STR({
+            unsafe {
+                core::str::from_utf8_unchecked(core::slice::from_raw_parts(
+                    payload.as_ptr(),
+                    payload.len(),
+                ))
+            }
+        })),
+        BYTES => Ok(Value::BYTES({
+            unsafe { core::slice::from_raw_parts(payload.as_ptr(), payload.len()) }
+        })),
+    }
+}
+
+#[cfg(feature = "std")]
+pub mod owned {
+    use serde::{Deserialize, Serialize};
+    use crate::protocol::AnswerCode;
+    use crate::ty::Value as NotOwnValue;
+    use crate::parser::Msg as NotOwnMsg;
+
+    #[derive(Debug, PartialEq, Eq, Clone)]
+    pub struct Msg(pub AnswerCode, pub String, pub Value);
+
+    #[allow(non_camel_case_types)]
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    #[derive(Serialize, Deserialize)]
+    pub enum Value {
+        UNIT(()),
+        BOOL(bool),
+        I32(i32),
+        I16(i16),
+        I8(i8),
+        U32(u32),
+        U16(u16),
+        U8(u8),
+        STR(String),
+        BYTES(Vec<u8>),
+    }
+
+    impl<'a> From<&'a Value> for NotOwnValue<'a> {
+        fn from(v: &'a Value) -> NotOwnValue<'a> {
+            use Value::*;
+            match v {
+                UNIT(v)  => NotOwnValue::UNIT(*v),
+                BOOL(v)  => NotOwnValue::BOOL(*v),
+                I32(v)   => NotOwnValue::I32(*v), 
+                I16(v)   => NotOwnValue::I16(*v), 
+                I8(v)    => NotOwnValue::I8(*v), 
+                U32(v)   => NotOwnValue::U32(*v), 
+                U16(v)   => NotOwnValue::U16(*v), 
+                U8(v)    => NotOwnValue::U8(*v), 
+                STR(v)   => NotOwnValue::STR(v), 
+                BYTES(v) => NotOwnValue::BYTES(v), 
+            }
         }
-        BOOL => {
-            Ok(Value::BOOL({
-                if payload[0] == 0 { false } else { true }
-            }))
+    }
+
+    impl<'a> From<NotOwnValue<'a>> for Value {
+        fn from(v: NotOwnValue<'a>) -> Value {
+            use NotOwnValue::*;
+            match v {
+                UNIT(v)  => Value::UNIT(v),
+                BOOL(v)  => Value::BOOL(v),
+                I32(v)   => Value::I32(v), 
+                I16(v)   => Value::I16(v), 
+                I8(v)    => Value::I8(v), 
+                U32(v)   => Value::U32(v), 
+                U16(v)   => Value::U16(v), 
+                U8(v)    => Value::U8(v), 
+                STR(v)   => Value::STR(String::from(v)), 
+                BYTES(v) => Value::BYTES(Vec::from(v)), 
+            }
         }
-        I32 => {
-            Ok(Value::I32({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        I16 => {
-            Ok(Value::I16({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        I8 => {
-            Ok(Value::I8({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        U32 => {
-            Ok(Value::U32({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        U16 => {
-            Ok(Value::U16({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        U8 => {
-            Ok(Value::U8({
-                unsafe { *(payload.as_ptr() as *const _)}
-            }))
-        }
-        STR => {
-            Ok(Value::STR({
-                unsafe { core::str::from_utf8_unchecked(core::slice::from_raw_parts(payload.as_ptr(), payload.len())) }
-            }))
-        }
-        BYTES => {
-            Ok(Value::BYTES({
-                unsafe { core::slice::from_raw_parts(payload.as_ptr(), payload.len()) }
-            }))
+    }
+
+    impl<'a> From<NotOwnMsg<'a>> for Msg {
+        fn from(m: NotOwnMsg<'a>) -> Msg {
+            let NotOwnMsg(code, path, value) = m;
+            let path = String::from(path);
+            let value = Value::from(value);
+            Msg(code, path, value)
         }
     }
 }
@@ -178,14 +223,13 @@ fn value_parser<'a>(payload: &'a[u8], ty_id: u8) -> Result<Value<'a>, ParserErro
 mod tests {
     use super::*;
     use core::convert::TryInto;
-    
     #[test]
     fn parse_req_short() {
-        let mut buf = [0x0u8;MAX_MSG_SZ];
+        let mut buf = [0x0u8; MAX_MSG_SZ];
         let msg_orig = Msg(
             AnswerCode::OK_WRITE,
             "/test/something",
-            Value::STR("wofwofwof")
+            Value::STR("wofwofwof"),
         );
         let req_sz = RequestBuilder::new(&mut buf)
             .code(msg_orig.0.try_into().unwrap())
@@ -193,20 +237,19 @@ mod tests {
             .payload(msg_orig.2)
             .build()
             .unwrap();
-        
         let mut parser = ParseMsg::new();
-        let msg = parser.try_parse(&buf[.. req_sz]).unwrap();
+        let msg = parser.try_parse(&buf[..req_sz]).unwrap();
 
         assert_eq!(msg, msg_orig)
     }
 
     #[test]
     fn parse_req_long() {
-        let mut buf = [0x0u8;MAX_MSG_SZ];
+        let mut buf = [0x0u8; MAX_MSG_SZ];
         let msg_orig = Msg(
             AnswerCode::OK_WRITE,
             "/test/something/somethingsomethingrlylong/приветкакдела",
-            Value::STR("/test/something/somethingsomethingrlylong/приветкакдела")
+            Value::STR("/test/something/somethingsomethingrlylong/приветкакдела"),
         );
         let _req_sz = RequestBuilder::new(&mut buf)
             .code(msg_orig.0.try_into().unwrap())
@@ -214,11 +257,9 @@ mod tests {
             .payload(msg_orig.2)
             .build()
             .unwrap();
-        
         let mut parser = ParseMsg::new();
-        
         let mut pos = 0x40;
-        let mut chunk = &buf[0 .. 0x40];
+        let mut chunk = &buf[0..0x40];
         let msg;
         loop {
             match parser.try_parse(chunk) {
@@ -226,9 +267,9 @@ mod tests {
                     msg = Some(r);
                     break;
                 }
-                Err(ParserError::NeedMoreData) => { 
+                Err(ParserError::NeedMoreData) => {
                     println!("NeedMoreData: pos: {}", pos);
-                    chunk = &buf[0 .. pos];
+                    chunk = &buf[0..pos];
                     pos += 0x40;
                 }
                 Err(e) => panic!("{:?}", e),
@@ -236,10 +277,6 @@ mod tests {
         }
 
         println!("Msg: {:?}", &msg.unwrap());
-        
         assert_eq!(msg.unwrap(), msg_orig)
     }
-
 }
-
-
